9d5f85b726897900ba1bc31ae1000f7f633448fc
Antony Cartwright <antonyccartwright@gmail.com>
2022-12-15 02:58:08 +0100

    Generate catalogue.txyt

--- build.sh
+++ build.sh
@@ -33,13 +33,18 @@ find 'GB Accounts 2023-24' -mindepth 1 -maxdepth 1 -type d -exec './build-zip.sh
 
 ls -lrt './build'
 
-cat './index.html' | sed '$d' | sed '$d' | sed '$d' | sed '$d' > './build/zips.html'
+cat './index.html' | sed '$d' | sed '$d' | sed '$d' | sed '$d' > './build/index.html'
 cd './build'
+echo "<ol>" >> './index.html'
+echo "Package,Platform,Status,Legacy" > './catalogue.csv'
 ls -1 *.zip | while read line; do
-  echo "<a href=\"$line\">$line</a><br/>" >> './zips.html'
-  echo "$line" >> './packages.txt'
+  echo "<li><a href=\"$line\">$line</a></li>" >> './index.html'
+  echo "$line" >> './index.txt'
+  catalogue_line="${line/) Excel/),Excel}"
+  echo "${catalogue_line/.zip/,released,unused}" >> './catalogue.csv'
 done
-echo "</body>" >> './zips.html'
-echo "</html>" >> './zips.html'
-cat './zips.html'
+echo "</ol>" >> './index.html'
+echo "</body>" >> './index.html'
+echo "</html>" >> './index.html'
+cat './index.html'
 cd ..
