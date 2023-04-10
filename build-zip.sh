44cafafd0794d1a1b17ac1cc19d05f58d02bd4cb
Antony Cartwright <antony.cartwright@awaze.com>
2023-04-10 19:04:26 +0200

    Extend build file for range of year ends

--- build-zip.sh
+++ build-zip.sh
@@ -7,27 +7,63 @@
 # e.g.
 #    find GB*/ -mindepth 1 -maxdepth 1 -type d -exec './build-zip.sh' {} \;
 
-# Parameters
-DESTINATION_DIR='../../build'
-SOURCE_DIR=${1?}
+# Functions
+function zip_source_folder_to_file {
+  local package_source_dir="$1"
+  local zip_destination_dir="$2"
+  local zip_destination_name="$3"
 
-# Constants
-SOURCE_NAME=$(basename "${SOURCE_DIR?}")
-BASE_DIR=$(pwd)
-
-if [[ ! -f "${SOURCE_DIR?}/DO NOT USE - WORK IN PROGRESS.txt" ]] ;
-then
-  echo "Zipping: [${SOURCE_DIR?}] To: ${DESTINATION_DIR?}/${SOURCE_NAME?}.zip"
-
-  # cd into the folder
-  cd "${SOURCE_DIR?}"
+  echo "Zipping: [${package_source_dir?}] To: ${zip_destination_dir?}/${zip_destination_name?}.zip"
 
   # Zip the contents (but not the .git folder)
-  rm -f "${DESTINATION_DIR?}/${SOURCE_NAME?}.zip"
-  zip -r "${DESTINATION_DIR?}/${SOURCE_NAME?}.zip" * -q -X -1 -x "*/.git/*" "*.sh"
+  rm -f "${zip_destination_dir?}/${zip_destination_name?}.zip"
+  zip -r "${zip_destination_dir?}/${zip_destination_name?}.zip" * -q -X -1 -x "*/.git/*" "*.sh"
+} 
+
+# Parameters
+zip_destination_dir='../../build'
+source_dir=${1?}
+
+# Constants
+source_name=$(basename "${source_dir?}")
+base_dir=$(pwd)
+
+if [[ ! -f "${source_dir?}/DO NOT USE - WORK IN PROGRESS.txt" ]] ;
+then
+
+  # cd into the folder
+  cd "${source_dir?}"
+
+  # If this is the Any package, generate multiple zips
+  if [[ $source_name == *"(Any)"* ]] ;
+  then
+    local package="GB Accounts Company"
+    local start_range="2024"
+    local end_range="2025"
+    local format="Excel 2007"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-04-30 (Apr${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-05-31 (May${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-06-30 (Jun${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-07-31 (Jul${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-08-31 (Aug${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-09-30 (Sep${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-10-31 (Oct${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-11-30 (Nov${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${start_range?}-12-31 (Dec${start_range:(-2)}) ${format?}"
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${end_range?}-01-31 (Jan${end_range:(-2)}) ${format?}"
+    # Lazy leap year check is correct between 2001 and 2099.
+    if ! (( ${end_range?} % 4 )) ;
+    then
+      zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${end_range?}-02-29 (Feb${end_range:(-2)}) ${format?}"
+    else
+      zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${package?} ${end_range?}-02-28 (Feb${end_range:(-2)}) ${format?}"
+    fi
+  else
+    zip_source_folder_to_file "${source_dir?}" "${zip_destination_dir?}" "${source_name?}"
+  fi
 
   # cd back
-  cd "${BASE_DIR?}"
+  cd "${base_dir?}"
 else
-  echo "Skipping: [${SOURCE_DIR?}] because file DO NOT USE - WORK IN PROGRESS.txt exists"
+  echo "Skipping: [${source_dir?}] because file DO NOT USE - WORK IN PROGRESS.txt exists"
 fi
